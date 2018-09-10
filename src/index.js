35f268253eca7b8907f7a163e47fe7d312fd7b51
Tarik Desire <tprdesire@gmail.com>
2018-09-09 20:23:12 -0400

    Added cheese-list component, async redux, wired to backend API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import CheeseList from './components/cheese-list';
+import { Provider } from 'react-redux';
+import store from './store';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={store}>
+  <CheeseList />
+</Provider>, document.getElementById('root'));
 registerServiceWorker();
